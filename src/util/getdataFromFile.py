30f22f8270923807170135e059bc6e6a50f965bf
Salah-boutayeb <boutayebsalaheddin@gmail.com>
2023-05-07 00:08:44 +0100

    return json formated recipes

--- src/util/getdataFromFile.py
+++
@@ -1,7 +0,0 @@
-f = open("C:\\Users\\bouta\\OneDrive\\Bureau\\hackathons\\Think AI\\src\\util\\data.txt", "r")
-lines = f.read()
-texts = []
-for line in lines:
-    texts.push({
-        "recipe": line.strip(),
-    })
